321a04f6e9ca14ae48c57650807c9fca04db8201
unknown <amitkumar.aher@gmail.com>
2021-10-12 13:46:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './header/index';
+import Footer from './footer/index';
+function App() {
+  return (
+    <div>
+   <Header />
+   <Footer />
+  </div>
+  );
+}
+
+export default App;
+  
